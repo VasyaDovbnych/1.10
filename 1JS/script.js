a69b0bea5a945e262090b61390a9c5caa19ee452
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2024-10-06 16:04:01 +0300

    2

---
+++ 1JS/script.js
@@ -0,0 +1,36 @@
+'use strict'
+
+// a = b * 2;
+
+// const one = 'JavaScript'.length;
+// console.log(one);
+// const two = 'Hello'.toUpperCase();
+// console.log(two);
+
+// let username;
+// console.log(username);
+
+// age = 15;
+// console.log(age);
+
+// let age = 15;
+// age = 20;
+// console.log(age);
+
+// const age = 15;
+// console.log(age);
+
+// const price = 10;
+// const guantity = 3;
+// let total = price * guantity;
+// console.log(total);
+// const priseNew = 50;
+
+// const name = 'Vasyl';
+// console.log('Username is', name);
+
+// const username = 'Hello, Vasyl';
+// alert (username);
+
+const isComing = confirm('Ти лох?');
+alert (isComing);
