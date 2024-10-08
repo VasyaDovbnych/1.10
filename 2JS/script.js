8024ae6dfd8693db24ae3ab741b6564e4cc1ec6a
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2024-10-08 20:52:41 +0300

    т

--- 2JS/script.js
+++ 2JS/script.js
@@ -1,1 +1,62 @@
 'use strict'
+// + - * / %
+// const x = 10;
+// const y = 3;
+// console.log(x * y);
+
+// let value = 5;
+// value += 10;
+// console.log(value);
+
+// оператори порівняння
+// > < <= >=
+// a>b
+// a>=b
+// a<b
+// a<=b
+// a==b
+// a != b      !=-не
+// a===b
+// a!==b
+
+// const x = 5;
+// const y = 7;
+// const a = 5;
+// const b =  '5';
+// console.log(x > y);
+// console.log(x < y);
+// console.log(x < a);
+// console.log(x <= a);
+// console.log(x === y);
+// console.log(x === a);
+// console.log(x === b);
+// console.log(x == b);
+ 
+// console.log(Number.parseInt('12px'));
+// console.log(Number.parseInt('10fnvj44kfn'));
+// console.log(Number.parseInt('34.554fmmkmmk'));
+// console.log(Number.parseInt('rfsfrfc'));
+
+// console.log(Number.parseFloat('12px'));
+// console.log(Number.parseFloat('10fnvj44kfn'));
+// console.log(Number.parseFloat('34.554fmmkmmk'));
+// console.log(Number.parseFloat('rfsfrfc'));
+
+// const NumberOne = Number('55');
+// console.log(Number.isNaN(NumberOne));
+
+// const NumberTwo = Number('hello');
+// console.log(Number.isNaN(NumberTwo));
+
+// console.log(0.1 + 0.2 === 0.3);
+// console.log(0.1 + 0.2);
+// console.log ((0.1 * 10 + 0.2 * 10) /  10);
+
+// console.log (Math.floor(1.8));
+// console.log (Math.ceil(1.3));
+// console.log (Math.round(2.3));
+// console.log (Math.max(20, 34, 12, 43));
+// console.log (Math.min(20, 34, 12, 43));
+// console.log (Math.pow(2, 4));
+// console.log (Math.random() * (10-1) + 1);
+console.log(Math.floor(Math.random() * 100) + 1);
