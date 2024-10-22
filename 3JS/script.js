c178d79b427f354b2a5789da98c94e2d8e0ceca5
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2024-10-22 16:55:29 +0300

    в

---
+++ 3JS/script.js
@@ -0,0 +1,32 @@
+'use strict'
+const Number = 15 + 15 + 15 + '15';
+console.log( typeof Number);
+
+
+const message = 'Hello JavaScript!';
+console.log(message.length);
+
+console.log(message.toLowerCase());
+
+console.log(message.toUpperCase());
+
+console.log(message.indexOf('Java'));
+
+console.log(message.indexOf('lo'));
+
+console.log(message.includes('hello'));
+
+console.log(message.includes('Hello'));
+
+const name = 'Vasyl';
+const age = 16;
+const mood = 'happy';
+
+const namMessage = 'My name is' + name + ', I*m' + age + ' years old end' + mood + '.';
+console.log(namMessage);
+
+const NamHours = 80 / 60;
+console.log(NamHours);
+const days = Math.floor(NamHours / 24);
+console.log(days);
+
