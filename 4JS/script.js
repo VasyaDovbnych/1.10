4edd66ff20d6b6b5c49c2d8f56e3db429445dda9
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2024-10-22 19:21:45 +0300

    d

---
+++ 4JS/script.js
@@ -0,0 +1,43 @@
+'use strict'
+// const Number = 20;
+// const result = Number > 10 && Number < 30;
+// console.log(result);
+
+
+// const number = 5;
+// const Result = number < 10 || number > 30;
+// console.log(Result);
+
+// let cost = 0;
+// const sub = 'pro';
+// if(sub === 'pro'){
+//     cost = 100;
+// }
+// console.log(cost);
+
+// let cost;
+// const sub = 'normal';
+// if(sub === 'free'){
+//     cost = 0;
+// } else if(sub === 'pro'){
+//     cost = 100;
+// } else if(sub === 'premium'){
+//     cost = 200;
+// }
+// console.log(cost);
+
+
+const age = 16;
+const type = age >=18 ? 'adolt' : 'child';
+console.log(type);
+
+
+
+
+const jsName = prompt('Яка "офіційна" назва JavaScript?');
+
+if (jsName === 'ECMAScript') {
+  alert('Правильно!');
+} else {
+  alert('Ви не знаєте? ECMAScript!');
+}
--- hm2/script.js
+++ hm2/script.js
@@ -1,1 +1,44 @@
 'use strict'
+let celsius = 25;
+let fahrenheit = (celsius * 9 / 5) + 32;
+console.log(fahrenheit);
+
+let days = 30;
+let hours = days * 24;
+let minutes = hours * 60;
+console.log(hours);
+console.log(minutes);
+
+let health = 100;
+let energy = 50;
+health = health - 20;
+energy = energy - 10;
+console.log("Здоров'я: " + health);
+console.log("Енергія: " + energy);
+
+let total = 500;
+let final = total * 0.90;
+console.log(final);
+
+let number = 7.89;
+let rounded = Math.floor(number);
+console.log(rounded);
+
+let numberString = "3.14";
+let decimalValue = parseFloat(numberString);
+console.log(decimalValue);
+
+let textNumber = "42";
+let finalNumber = parseInt(textNumber);
+console.log(finalNumber);
+
+let numberOne = 16;
+let finalRoot = Math.sqrt(numberOne);
+console.log(finalRoot);
+
+let num = 25;
+let numOne = "30";
+let resultOne = parseInt(numOne);
+console.log(resultOne);
+let resultTwo = num.toString();
+console.log(resultTwo);
