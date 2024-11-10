d3c2a6e9d0f2f53c8f42c366a8002919061a2a33
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2024-11-10 18:59:38 +0200

    s

---
+++ hm6/script.js
@@ -0,0 +1,37 @@
+// 'use strict'
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     i = i + 1;
+// }
+
+// for (let i = 2; i <= 20; i = i + 1) {
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i = i + 1) {
+//     console.log("7 * " + i + " = " + (7 * i));
+// }
+
+// let n = prompt("Введите число n:");
+// let i = 1;
+// while (true) {
+//     if (i >= n) {
+//         break;
+//     }
+//     console.log(i);
+//     i = i + 1;
+// }
+
+// let i = 1;
+// while (i <= 20) {
+//     if (i % 3 === 0) {
+//         i = i + 1;
+//         continue;
+//     }
+//     console.log(i);
+//     i = i + 1;
+// }
