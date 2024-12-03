41b7148b0f5021d38fadf53ef606abc11410b450
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2024-12-03 18:01:01 +0200

    в

--- 8JS/script.js
+++ 8JS/script.js
@@ -1,3 +1,7 @@
 'use strict'
 const clienth = ['bob', 'Nik', 'Tom', 'Poly'];
 console.log(clienth.slice(1,3));
+
+const name = ['Nik', 'Tom', 'Pes', 'Kovach']
+name.splice(1, 1, 'Poly')
+console.log(name)
---
+++ 9JS/script.js
@@ -0,0 +1,5 @@
+'use strict'
+const add = function(a, b, c) {
+    return a + b + c;
+}
+console.log(add(5, 4, 3));
