b63ad90ab1a709566071c9c9a2d20d536f2202aa
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2024-12-10 16:50:12 +0200

    Update script.js
    к

--- 9JS/script.js
+++ 9JS/script.js
@@ -1,5 +1,28 @@
 'use strict'
-const add = function(a, b, c) {
-    return a + b + c;
-}
-console.log(add(5, 4, 3));
+// const add = function(a, b, c) {
+//     return a + b + c;
+// }
+// console.log(add(5, 4, 3));
+
+
+
+
+
+
+// const a = 3;
+// const b = 4;
+// const k = Math.sqrt(Math.pow( a, 2 ) + Math.pow(b, 2));
+// console.log(k);
+
+
+
+const randoms = Math.floor(Math.random() * (19 - 3 + 1)) + 3;
+console.log(randoms)
+
+
+const c = parseFloat(prompt('Ввидіть цифру'));
+const k = parseFloat(prompt('Ввидіть цифру'));
+const number = c + k;
+alert(`Сума цих чисел ${ number }` );
+
+// a2 * b2 = c2
