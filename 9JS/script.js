72b818f7afe14665b66268d1c49172f3b57e6b5d
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2024-12-24 16:53:32 +0200

    ч

--- 9JS/script.js
+++ 9JS/script.js
@@ -30,23 +30,120 @@
 
 
 
-const filter = function (array, test) {
-    const filteredElements = [];
-    for (const element of array){
-        const passed = test(element);
-        if (passed){
-            filteredElements.push(element)
-        }
+// const filter = function (array, test) {
+//     const filteredElements = [];
+//     for (const element of array){
+//         const passed = test(element);
+//         if (passed){
+//             filteredElements.push(element)
+//         }
+//     }
+//     return filteredElements;
+// }
+
+
+// const fruits = [
+//     {name: 'apples', quantity: 300, isFresh: true},
+//     {name: 'ananas', quantity: 150, isFresh: true},
+//     {name: 'kiwis', quantity: 550, isFresh: false}
+// ]
+
+// const freshFruits = filter(fruits, fruit => fruit.isFresh);
+// console.log(freshFruits);
+
+
+// const add = (a, b, c) => a + b + c;
+
+// const fn = x => x * 2;
+
+// const fn2 = () => {
+//     console.log('hello')
+// }
+// console.log(fn2)
+
+// const fn3 = (a, b, c) => {
+//     return a + b + c;
+// }
+
+// const addArr =  (...args) => {
+//     console.log(args)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const hotel = {
+    name: 'Hittom',
+    stars: 5,
+    capacity: 250,
+    senMessege: function(){
+        console.log('Welcome')
+    },
+
+    senMessege(){
+        console.log('Welcome')
     }
-    return filteredElements;
+}
+console.log(hotel);
+
+
+const person = {
+    name: 'Tom',
+    age: 12,
+    gender: 'boy',
+
+    describe(){
+        console.log(`Ім'я: ${this.name} Вік: ${this.age} Стать: ${this.gender}`)
+    }
+} 
+
+console.log(person.describe());
+
+
+
+
+const students = [ {
+    name: 'One',
+    age: 11,
+    gender: 'boy',
+    grade: 1
+}, {
+    name: 'Two',
+    age: 17,
+    gender: 'boy',
+    grade: 2
 }
 
-
-const fruits = [
-    {name: 'apples', quantity: 300, isFresh: true},
-    {name: 'ananas', quantity: 150, isFresh: true},
-    {name: 'kiwis', quantity: 550, isFresh: false}
 ]
-
-const freshFruits = filter(fruits, fruit => fruit.isFresh);
-console.log(freshFruits);
+function count (students){
+    let count = 0;
+    let total = 0;
+    for( let i = 0; i < students.length; i++){
+        total += students[i].grade;
+        count++;
+    }
+    return count === 0 ? 0 : total/count;
+}
+ const average = count(students)
+ console.log(`Середній бал студентів ${average}`);
