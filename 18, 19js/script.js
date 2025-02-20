a3343578f9bec734f430ed5326e392626e894a60
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2025-02-20 20:38:13 +0200

    Update script.js

--- 18, 19js/script.js
+++ 18, 19js/script.js
@@ -9,18 +9,48 @@ const players = [
 
 
 
-const user = {
-    name: "Іван",
-    age: 25,
+// const user = {
+//     name: "Іван",
+//     age: 25,
+// };
+
+
+// const baseSalary = 30000;
+// const overtime = 10;
+// const rate = 20;
+
+// const getWage = (baseSalary, overtime, rate) => {
+//   return baseSalary + overtime * rate;
+// };
+
+// getWage(baseSalary, overtime, rate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Car {
+  constructor(model, price, color){
+    this.model = model
+    this.price = price
+    this.color = color
+  }
 };
-
-
-const baseSalary = 30000;
-const overtime = 10;
-const rate = 20;
-
-const getWage = (baseSalary, overtime, rate) => {
-  return baseSalary + overtime * rate;
-};
-
-getWage(baseSalary, overtime, rate);
+const car1 = new Car ({model:'Porsche Macan 2020', price:500000, color:'red'});
+const car2 = new Car ({model:'Porsche Macan 2015', price:900000, color:'white'});
+const car3 = new Car ({model:'Porsche Macan 2010', price:400000, color:'black'});
+console.log(car1);
+console.log(car2);
+console.log(car3);
