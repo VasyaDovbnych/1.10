f624e0d4c528d7ea0eef459889dbeba2f0f1cfd2
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2025-03-11 19:19:19 +0200

    в

--- DOM/script.js
+++ DOM/script.js
@@ -1,3 +1,9 @@
 'use strict'
 const listEl = document.querySelector("ul")
 console.log(listEl)
+
+const firstItemEl = listEl.firstElementChild
+console.log(firstItemEl)
+
+const listElOne = firstItemEl.parentElement
+console.log(listElOne)
