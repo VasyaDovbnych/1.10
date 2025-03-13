fffc3fc6757d0d126ae6910732cfebf8c1e4f182
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2025-03-13 19:05:24 +0200

    к

--- DOM/script.js
+++ DOM/script.js
@@ -7,3 +7,17 @@ console.log(firstItemEl)
 
 const listElOne = firstItemEl.parentElement
 console.log(listElOne)
+
+
+
+
+
+
+
+
+
+const titleEl = document.createElement('h1')
+titleEl.classList.add('page-title')
+titleEl.textContent = 'My first element'
+titleEl.style.color = 'red'
+console.log(titleEl)
