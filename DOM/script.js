873ed9bc812c9d682380627f666b2af396884c99
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2025-03-25 19:17:36 +0200

    и

--- DOM/script.js
+++ DOM/script.js
@@ -1,14 +1,12 @@
 'use strict'
-const listEl = document.querySelector("ul")
-console.log(listEl)
+// const listEl = document.querySelector("ul")
+// console.log(listEl)
 
-const firstItemEl = listEl.firstElementChild
-console.log(firstItemEl)
+// const firstItemEl = listEl.firstElementChild
+// console.log(firstItemEl)
 
-const listElOne = firstItemEl.parentElement
-console.log(listElOne)
-
-
+// const listElOne = firstItemEl.parentElement
+// console.log(listElOne)
 
 
 
@@ -16,8 +14,29 @@ console.log(listElOne)
 
 
 
-const titleEl = document.createElement('h1')
-titleEl.classList.add('page-title')
-titleEl.textContent = 'My first element'
-titleEl.style.color = 'red'
-console.log(titleEl)
+
+
+// const titleEl = document.createElement('h1')
+// titleEl.classList.add('page-title')
+// titleEl.textContent = 'My first element'
+// titleEl.style.color = 'red'
+// console.log(titleEl)
+
+// const imageEl = document.createElement('img')
+// imageEl.src = 'https://cdn.pixabay.com/photo/2018/07/26/07/45/valais-3562988_1280.jpg'
+// imageEl.alt = 'valais-alpine-mountains-glacier'
+// console.log(imageEl);
+
+
+
+
+
+const inpuEl = document.querySelector('.js-input');
+const checkboxEl = document.querySelector('.js-license');
+
+
+
+
+
+
+
