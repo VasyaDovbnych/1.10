9055c7ac95b882815188570491db021a5a84c094
VasyaDovbnych <153006164+VasyaDovbnych@users.noreply.github.com>
2025-05-29 19:32:05 +0300

    і

--- lodash/script.js
+++ lodash/script.js
@@ -1,11 +1,11 @@
 'use strict';
-const coords = document.querySelector('.js-coords');
-let mouse = 0;
-window.addEventListener('mousemove', (event) =>{
-    mouse += 1;
-    coords.textContent = `
-    Кількість викликів onMouseMove: ${mouse},
-    X: ${event.clientX},
-    Y:${event.clientY}
-  `;
-})
+// const coords = document.querySelector('.js-coords');
+// let mouse = 0;
+// window.addEventListener('mousemove', (event) =>{
+//     mouse += 1;
+//     coords.textContent = `
+//     Кількість викликів onMouseMove: ${mouse},
+//     X: ${event.clientX},
+//     Y:${event.clientY}
+//   `;
+// })
